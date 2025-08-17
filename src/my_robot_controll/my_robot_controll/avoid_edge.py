48ebd8ec26bb93e8d41bcf73108c3ee6da8ad158
nithinganesh1 <nithingganesh1@gmail.com>
2025-08-17 09:44:29 +0530

    added service

--- src/my_robot_controll/my_robot_controll/avoid_edge.py
+++ src/my_robot_controll/my_robot_controll/avoid_edge.py
@@ -3,9 +3,12 @@ import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
+from turtlesim.srv import SetPen
+from functools import partial
 
 class Clossed_Loop(Node):
     def __init__(self):
+        self.previous_x_ = 0
         super().__init__('edge_avoiding_turtle')
         self.get_logger().info("Started turtle contorl")
         self.publisher_ = self.create_publisher(Twist,'/turtle1/cmd_vel',10)
@@ -17,7 +20,39 @@ class Clossed_Loop(Node):
             cmd.angular.z = 0.9
         else:
             cmd.linear.x = 5.0
+        
+        if msg.x >= 5.5 and self.previous_x_ <= 5.5:
+            self.previous_x_= msg.x
+            self.get_logger().info('Changed Color to Red')
+            self.call_set_pen_sev(255,0,0,5,0)
+        elif msg.x <= 5.5 and self.previous_x_ >= 5.5:
+            self.previous_x_ = msg.x
+            self.get_logger().info("Changed Color to Green")
+            self.call_set_pen_sev(0,255,0,5,0)
+
         self.publisher_.publish(cmd)
+    
+    def call_set_pen_sev(self,r,g,b,width,off): 
+        clinet = self.create_client(SetPen,'/turtle1/set_pen')
+        while not clinet.wait_for_service(1.0): # wait for 1 second for service is available or not
+            self.get_logger().warn("Waiting for the response")
+
+        request = SetPen.Request()
+        request.r = r
+        request.g = g
+        request.b = b
+        request.width = width
+        request.off = off
+
+        future = clinet.call_async(request=request)
+        future.add_done_callback(partial(self.callback_set_pen))
+
+    def callback_set_pen(self,future): # for handling request
+        try:
+            response = future.result()
+        except Exception as e:
+            self.get_logger().error(f"Service called faild {e}")
+        
 
 def main(args=None):
     rclpy.init(args=args)
